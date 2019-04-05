e19bca6fe067c65c90d6518eb439c529b34217c0
beaux1rome <beaux1rome@gmail.com>
2019-04-05 11:00:32 -0500

    change name

--- RuneTranslator/Program.cs
+++ RuneTranslator/Program.cs
@@ -16,7 +16,7 @@ namespace RuneTranslator
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new Form1() { Text = "Rune Translator"});
             
         }
     }
